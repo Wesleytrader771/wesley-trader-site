bb2dcb59f5b446241743c9387ed38f86572abd85
Wesley Trader <wesley.trader1272@gmail.com>
2026-03-18 15:02:22 -0300

    real dev 18/03/2026 15:02:21,86

--- bot_commit.bat
+++ bot_commit.bat
@@ -7,11 +7,9 @@ set count=1
 
 :loop
 
-:: cria arquivo diferente (mais real)
 set filename=dev_%random%_%count%.txt
-echo Conteudo real %random% %time% > %filename%
+echo Conteudo %random% %time% > %filename%
 
-:: altera outro arquivo também
 echo Update %random% %date% %time% >> atividade.txt
 
 git add .
@@ -20,8 +18,8 @@ git push
 
 set /a count+=1
 
-:: tempo MAIS humano (2 a 5 minutos)
+:: tempo aleatório (2 a 5 minutos)
 set /a tempo=(%random% %% 180) + 120
 timeout /t %tempo% >nul
 
-if %count% LEQ 10 goto loop
+goto loop
