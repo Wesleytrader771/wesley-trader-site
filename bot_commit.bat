82b978fd9d01aad10d113b000f2b91c444c3ec15
Wesley Trader <wesley.trader1272@gmail.com>
2026-03-18 14:11:00 -0300

    commit real 1

--- bot_commit.bat
+++ bot_commit.bat
@@ -1,11 +1,20 @@
 @echo off
-cd /d C:\projeto\meu-projeto
+cd /d "C:\projeto\meu-projeto"
 
 git pull
 
-for /l %%x in (1, 1, 50) do (
-echo update %%x >> log.txt
+set count=1
+
+:loop
+echo Commit numero %count% >> log.txt
+echo %random% >> log.txt
+
 git add .
-git commit -m "auto commit %%x"
+git commit -m "commit real %count%"
 git push
-)
+
+set /a count+=1
+
+timeout /t 20 >nul
+
+if %count% LEQ 30 goto loop
